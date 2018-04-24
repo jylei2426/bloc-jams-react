2a9642040ed065d8da7a3a74057b2e393c40fe25
Jenny Lei <jylei2426@live.carlow.edu>
2018-04-24 18:17:22 -0400

    routing assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
+import Album from './components/Album';
 import Library from './components/Library';
 
 
@@ -12,12 +13,14 @@ class App extends Component {
       <header>
         <nav>
           <Link to='/'>Landing</Link>
+          <Link to='/Album'>Album</Link>
           <Link to='/library'>Library</Link>
         </nav>
         <h1>Bloc Jams</h1>
        </header>
        <main>
         <Route exact path="/" component={Landing} />
+        <Route path="/Album" component={Album} />
         <Route path="/library" component={Library} />
        </main>
       </div>
